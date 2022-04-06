5fc0d893166b43d6b16d20a86f27d130d804cbed
Bryan <bryanlima200@outlook.com>
2022-04-05 23:20:54 -0300

    Removido propriedade Teste da entidade Aluno e gerado migration RetiraTeste

--- src/DevIO.UI.Site/Models/Aluno.cs
+++ src/DevIO.UI.Site/Models/Aluno.cs
@@ -16,6 +16,5 @@ namespace DevIO.UI.Site.Models
         public string Nome { get; set; }
         public string Email { get; set; }
         public DateTime DataNascimento { get; set; }
-        public string Teste { get; set; }
     }
 }
